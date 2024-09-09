ca72e637b27ff08e86e2dbd0ef732caaf54b9866
KangDagyeom <thang7781@gmail.com>
2024-09-09 14:20:17 +0700

    Logn and Sign Up

---
+++ src/Test/LoginForm.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class LoginForm {
+    public static void main(String[] args) {
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+}
